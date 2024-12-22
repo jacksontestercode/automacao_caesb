a86a07d6dfc6506a3ee2dff1bade637143c72fd8
jacksontestercode <jacksonfla6@gmail.com>
2024-12-22 18:00:12 -0300

    Organização das etruturas das pastas

--- cypress/e2e/testes_automatizados/automacao_caesb/GCOM/atendimento_pesquisar_atendimento.cy.js
+++ cypress/e2e/testes_automatizados/automacao_caesb/GCOM/atendimento_pesquisar_atendimento.cy.js
@@ -2,12 +2,13 @@
 
 describe('GCOM - Atendimento Comercial e Serviços', function(){
 // O beforeEach será executado antes de cada teste (it)
-    this.beforeEach(function(){
+    beforeEach(function(){
         cy.visit('https://t2.homolog.caesb.df.gov.br/seguranca/app/login')
     })
     it('Fazer login', function(){
-        cy.get('#j_username').type('jacksonmendes')
-        cy.get('#j_password').type('Administrador)))')
-        cy.get('#btEntrar').click()
+         // Realiza o login
+        cy.get('#j_username').type('jacksonmendes') // Seleciona o campo de entrada de username pelo ID e digita o nome de usuário
+        cy.get('#j_password').type('Administrador)))') // Seleciona o campo de entrada de senha pelo ID e digita a senha
+        cy.get('#btEntrar').click() // Seleciona o botão de login pelo ID e clica nele
     })
 })
