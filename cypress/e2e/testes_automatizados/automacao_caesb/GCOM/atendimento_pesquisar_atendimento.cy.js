1097eb824c2045040354765a0310b25a059d660b
jacksontestercode <jacksonfla6@gmail.com>
2024-12-22 15:55:05 -0300

    Criando a página de visita do ambiente de teste T2

--- cypress/e2e/testes_automatizados/automacao_caesb/GCOM/atendimento_pesquisar_atendimento.cy.js
+++ cypress/e2e/testes_automatizados/automacao_caesb/GCOM/atendimento_pesquisar_atendimento.cy.js
@@ -1,1 +1,7 @@
 /// <reference types = 'Cypress' />
+
+describe('GCOM - Atendimento Comercial e Serviços', function(){
+    this.beforeEach(function(){
+        cy.visit('https://t2.homolog.caesb.df.gov.br/seguranca/app/login')
+    })
+})
