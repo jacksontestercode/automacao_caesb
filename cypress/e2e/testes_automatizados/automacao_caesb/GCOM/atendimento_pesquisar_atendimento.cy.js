0f11e045681a1c7c62684081c21436adb54bcdbd
jacksontestercode <jacksonfla6@gmail.com>
2024-12-22 17:39:37 -0300

    Criado a função login

--- cypress/e2e/testes_automatizados/automacao_caesb/GCOM/atendimento_pesquisar_atendimento.cy.js
+++ cypress/e2e/testes_automatizados/automacao_caesb/GCOM/atendimento_pesquisar_atendimento.cy.js
@@ -1,7 +1,13 @@
 /// <reference types = 'Cypress' />
 
 describe('GCOM - Atendimento Comercial e Serviços', function(){
+// O beforeEach será executado antes de cada teste (it)
     this.beforeEach(function(){
         cy.visit('https://t2.homolog.caesb.df.gov.br/seguranca/app/login')
+    })
+    it('Fazer login', function(){
+        cy.get('#j_username').type('jacksonmendes')
+        cy.get('#j_password').type('Administrador)))')
+        cy.get('#btEntrar').click()
     })
 })
